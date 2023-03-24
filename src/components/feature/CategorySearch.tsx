1b184ce82795545c3ec9e18fea3f14b14eb10921
sasaki mai <mai.sasaki@rakus-partners.co.jp>
2023-03-24 16:13:36 +0900

    カテゴリ検索 不要ファイル削除

--- src/components/feature/CategorySearch.tsx
+++ src/components/feature/CategorySearch.tsx
@@ -2,10 +2,17 @@ import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import ItemCategorySelect from "../listing-form/ItemCategorySelect";
 import { Items } from "../../types/type";
+import { Button } from "@mui/material";
 
-const CategorySearch = () => {
-  const [itemCategory, setItemcategory] = useState("");
-  const [items, setItems] = useState<Items[]>([]);
+type CategorySerchProps = {
+  itemCategory: string;
+  setItemcategory: React.Dispatch<React.SetStateAction<string>>;
+  items: Items[];
+  setItems: React.Dispatch<React.SetStateAction<Items[]>>;
+};
+
+const CategorySearch = (props: CategorySerchProps) => {
+  const { itemCategory, setItemcategory, items, setItems } = props;
 
   //Item情報
   useEffect(() => {
@@ -14,16 +21,67 @@ const CategorySearch = () => {
       const data = await res.json();
       setItems(data);
     })();
-  }, []);
-
-  console.log(items);
+  }, [itemCategory]);
+  //選んだカテゴリーの商品を表示
+  
+  console.log(items,"items!!");
+  console.log(itemCategory)
+  
+  const onItemSelect = () => {
+    const filteredItems = items.filter((item) => item.category === itemCategory);
+    setItems(filteredItems);
+  };
+  
+  const onSelectReset=()=>{
+    (async () => {
+      const res = await fetch("http://localhost:8000/items");
+      const data = await res.json();
+      setItems(data);
+    })();
+    setItemcategory("")
+  }
 
   return (
-    <Box>
+    <Box display="flex" alignItems="center" justifyContent="flex-end">
       <ItemCategorySelect
         itemCategory={itemCategory}
         setItemCategory={setItemcategory}
       />
+      <Box ml={2}>
+        <Button
+          sx={{
+            backgroundColor: "#82ae46",
+            p: 0,
+            color: "#fff",
+            fontWeight: "bold",
+            mt: 1,
+            "&:hover": {
+              backgroundColor: "#669934",
+            },
+          }}
+          onClick={onItemSelect}
+        >
+          検索
+        </Button>
+      </Box>
+      <Box ml={2}>
+        <Button
+          sx={{
+            backgroundColor: "#82ae46",
+            p: 0,
+            color: "#fff",
+            fontWeight: "bold",
+            mt: 1,
+            mr: 2,
+            "&:hover": {
+              backgroundColor: "#669934",
+            },
+          }}
+          onClick={onSelectReset}
+        >
+          リセット
+        </Button>
+      </Box>
     </Box>
   );
 };
