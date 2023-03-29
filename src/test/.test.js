bf48bf5e8ea053737c36577c430226c7c9000147
sasaki mai <mai.sasaki@rakus-partners.co.jp>
2023-03-29 15:45:09 +0900

    EmailInput,PasswordInputテスト

--- src/test/.test.js
+++ src/test/.test.js
@@ -3,64 +3,15 @@ import MembersInfoEdit from "../pages/users/MembersInfoEdit";
 import React from "react";
 import '@testing-library/jest-dom';
 
+//テストコードが読み込まれているかの確認で使用
 test('two plus two is four', () => {
   expect(2 + 2).toBe(4);
 });
 
+//送信ボタンがあるかどうか
 test('renders Button ', () => {
   const { getByTestId } =render(<MembersInfoEdit />);
   const submitButton = getByTestId('submit-button');
   expect(submitButton).toBeInTheDocument();
 });
 
-// test("form input values are saved in state", () => {
-//   render(<Login />);
-//   const emailInput = screen.getByLabelText("メールアドレス");
-//   const passwordInput = screen.getByLabelText("パスワード");
-//   fireEvent.change(emailInput, { target: { value: "test@test.com" } });
-//   fireEvent.change(passwordInput, { target: { value: "aaaaaaaa" } });
-//   expect(emailInput.value).toBe("test@test.com");
-//   expect(passwordInput.value).toBe("aaaaaaaaa");
-// });
-
-// test("login is successful and cookie is set if email and password match", async () => {
-//   const mockUser = [{ email: "test@test.com", password: "aaaaaaaa", id: 1 }];
-//   jest.spyOn(global, "fetch").mockImplementation(() =>
-//     Promise.resolve({
-//       json: () => Promise.resolve(mockUser),
-//     })
-//   );
-
-//   render(<Login />);
-//   const emailInput = screen.getByLabelText("メールアドレス");
-//   const passwordInput = screen.getByLabelText("パスワード");
-//   fireEvent.change(emailInput, { target: { value: "test@test.com" } });
-//   fireEvent.change(passwordInput, { target: { value: "aaaaaaaa" } });
-//   fireEvent.click(screen.getByText("ログイン"));
-
-//   // cookieが設定されていることを確認する
-//   expect(document.cookie).toContain("data");
-
-//   global.fetch.mockRestore();
-// });
-
-// test("error message is displayed if email or password do not match", async () => {
-//   const mockUser = [{ email: "test@test.com", password: "aaaaaaaa", id: 1 }];
-//   global.fetch = jest.fn().mockImplementation(() =>
-//   Promise.resolve({
-//     json: () => Promise.resolve(mockUser),
-//   })
-// );
-
-//   render(<Login />);
-//   const emailInput = screen.getByLabelText("Email");
-//   const passwordInput = screen.getByLabelText("Password");
-//   fireEvent.change(emailInput, { target: { value: "test@test.com" } });
-//   fireEvent.change(passwordInput, { target: { value: "aaaaaaaaa" } });
-//   fireEvent.click(screen.getByText("ログイン"));
-
-//   // エラーメッセージが表示されていることを確認する
-//   expect(screen.getByText("＊入力内容を確認してください＊")).toBeInTheDocument();
-
-//   global.fetch.mockRestore();
-// });
